ba12550d14099ce72750c0b5669659ada381fb9b
Viktor64 <ecogorod64@mail.ru>
2024-09-25 19:19:08 +0400

    Для теста в классе Main созданы: две задачи, а также эпик с двумя подзадачами и эпик с одной подзадачей. Вывод списка эпиков, задач и подзадач реализован через System.out.println(..). Реализованы изменения статусов созданных объектов, вывод новых значений статусов. Проверка, что статус задачи и подзадачи сохранился, а статус эпика рассчитался по статусам подзадач. Попробуем удалить одну из задач и один из эпиков, выводим результат.

---
+++ src/Epic.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+class Epic extends Task {
+    private List<Subtask> subtasks = new ArrayList<>();
+
+    public Epic(String name, String description, TaskStatus status) {
+        super(name, description, status);
+    }
+
+    public void addSubtask(Subtask subtask) {
+        subtasks.add(subtask);
+    }
+
+    public void removeSubtask(int subtaskId) {
+        subtasks.removeIf(subtask -> subtask.getId() == subtaskId);
+    }
+
+    public List<Subtask> getSubtasks() {
+        return subtasks;
+    }
+
+    public void updateSubtask(Subtask subtask) {
+        for (int i = 0; i < subtasks.size(); i++) {
+            if (subtasks.get(i).getId() == subtask.getId()) {
+                subtasks.set(i, subtask);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Epic {" +
+                "id= " + getId() +
+                ", name= '" + getName() + '\'' +
+                ", description= " + getDescription() +
+                ", status= " + getStatus() +
+                ", subtasks= " + subtasks +
+                '}';
+    }
+}
